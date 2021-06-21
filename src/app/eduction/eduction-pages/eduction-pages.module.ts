5dac75137e24a74f9b6011454c249a35e04d47a2
radwa <radwa@programmingmap.com>
2021-06-21 12:44:50 +0200

    packages

--- src/app/eduction/eduction-pages/eduction-pages.module.ts
+++ src/app/eduction/eduction-pages/eduction-pages.module.ts
@@ -6,10 +6,12 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CoursesComponent } from './components/courses/courses.component';
 import { EductionPagesComponent } from './containers/eduction-pages/eduction-pages.component';
+import { FilterComponent } from './components/courses/filter/filter.component';
+import { ListComponent } from './components/courses/list/list.component';
 
 
 @NgModule({
-  declarations: [ NavbarComponent, HeaderComponent, CoursesComponent, EductionPagesComponent],
+  declarations: [ NavbarComponent, HeaderComponent, CoursesComponent, EductionPagesComponent, FilterComponent, ListComponent],
   imports: [
     CommonModule,
     EductionPagesRoutingModule
